cd24526847be484e58a211e3067ff63903d326c7
marsdenmadoka <marsmadoka98@gmail.com>
2022-09-29 21:37:54 +0300

    removed unused code

--- app/src/main/java/com/madoka/movieshop/ui/components/PlayingNowItem.kt
+++ app/src/main/java/com/madoka/movieshop/ui/components/PlayingNowItem.kt
@@ -1,22 +1,23 @@
 package com.madoka.movieshop.ui.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.Dimension
+import coil.compose.AsyncImage
 import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
 import com.madoka.domain.model.Movie
 
 
@@ -26,34 +27,30 @@ fun TopPlayingNowSectionItem(
     movie: Movie,
     onItemClick: () -> Unit
 ) {
-    val painter = rememberImagePainter(
-        data = movie.posterPath,
-        builder = {
-            crossfade(true)
-        }
-    )
 
     Box {
         ConstraintLayout(modifier = Modifier.fillMaxSize()) {
             val (imageMovieCover, textMovieTitle, ratingBarRanking) = createRefs()
 
             //region Movie Cover Image
-            Image(
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(movie.posterPath)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = "Movie Image",
                 modifier = Modifier
                     .fillMaxSize()
-                    .constrainAs(imageMovieCover) {}
+                    .constrainAs(imageMovieCover){}
                 /**                 .placeholder(
                 visible = false,
                 color = Color.Gray,
                 //highlight = PlaceholderHighlight.shimmer(highlightColor = Color.White)
-                ) **/
-                ,
+                ) **/,
                 alignment = Alignment.Center,
                 contentScale = ContentScale.Crop,
-                painter = painter,//painterResource(id = faketopitem.Image),
-                contentDescription = null
-            )
 
+                )
             //region Movie Title
             Text(
                 modifier = Modifier
@@ -72,9 +69,37 @@ fun TopPlayingNowSectionItem(
                 textAlign = TextAlign.Start,
                 // color = dominantTextColor
             )
-
         }
-
-
     }
 }
+
+
+
+
+
+
+//
+//val painter = rememberImagePainter(
+//    data = movie.posterPath,
+//    builder = {
+//        crossfade(true)
+//    }
+//)
+//
+//
+//
+//Image(
+//modifier = Modifier
+//.fillMaxSize()
+//.constrainAs(imageMovieCover) {}
+///**                 .placeholder(
+//visible = false,
+//color = Color.Gray,
+////highlight = PlaceholderHighlight.shimmer(highlightColor = Color.White)
+//) **/
+//,
+//alignment = Alignment.Center,
+//contentScale = ContentScale.Crop,
+//painter = painter,//painterResource(id = faketopitem.Image),
+//contentDescription = null
+//)
