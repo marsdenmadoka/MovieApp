fe30311db7e90961aedc80c265ee39931d2b2b25
marsdenmadoka <marsmadoka98@gmail.com>
2022-11-08 10:26:05 +0300

    writting gson converters

--- data/src/main/java/com/madoka/cache/converters/MovieEntityConverter.kt
+++ data/src/main/java/com/madoka/cache/converters/MovieEntityConverter.kt
@@ -1,9 +1,27 @@
 package com.madoka.cache.converters
 
+import androidx.room.TypeConverter
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.madoka.cache.models.MovieEntity
 
 class MovieEntityConverter {
     private val gson = Gson()
 
     @TypeConverter
+    fun from(movieEntity: List<MovieEntity>): String? {
+        if (movieEntity.isNullOrEmpty()) return null
+
+        val type = object : TypeToken<List<MovieEntity>?>() {}.type
+        return gson.toJson(movieEntity, type)
+
+    }
+
+    @TypeConverter
+    fun to(movieEntityString:String?):List<MovieEntity>?{
+        if (movieEntityString.isNullOrEmpty()) return null
+
+        val type = object : TypeToken<List<MovieEntity>?>() {}.type
+        return gson.fromJson(movieEntityString, type)
+    }
 }
---
+++ data/src/main/java/com/madoka/cache/models/MovieEntity.kt
@@ -0,0 +1,42 @@
+package com.madoka.cache.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Movies Table")
+data class MovieEntity(
+    val adult: Boolean?,
+
+    val backdropPath: String?,
+
+    val genreIds: List<Int>?,
+
+    val id: Int?,
+
+    val originalLanguage: String?,
+
+    val originalTitle: String?,
+
+    val overview: String?,
+
+    val popularity: Double?,
+
+    val posterPath: String?,
+
+    val releaseDate: String?,
+
+    val title: String?,
+
+    val video: Boolean?,
+
+    val voteAverage: Double?,
+
+    val voteCount: Int?,
+
+    val category: String?,
+
+    val isFavorite: Boolean?,
+
+    @PrimaryKey(autoGenerate = true)
+    val cacheId:Int=0
+)
